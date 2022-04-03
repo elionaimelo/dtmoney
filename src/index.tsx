ba26fd62e885abc956a5b8086a2a8a958412184d
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2022-04-03 12:19:36 -0300

    Configurando miragejs, api fake

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createServer } from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Gás de cozinha',
+          value: -50.00,
+          type: 'withdraw',
+          category: 'Saúde',
+          date: '20/04/2020'
+        },
+        {
+          id: 2,
+          title: 'Suplementos',
+          value: -30.00,
+          type: 'withdraw',
+          category: 'Saúde',
+          date: '20/04/2020'
+        },
+        {
+          id: 3,
+          title: 'Salário',
+          value: 1200.00,
+          type: 'deposit',
+          category: 'Salário',
+          date: '20/04/2020'
+        },
+        {
+          id: 4,
+          title: 'Aluguel',
+          value: -10000.00,
+          type: 'withdraw',
+          category: 'Imóveis',
+          date: '20/04/2020'
+        }
+      ]
+    })
+  }
+})
+
 
 ReactDOM.render(
   <React.StrictMode>
